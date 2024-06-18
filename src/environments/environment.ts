960fd4815eefd413fac7dfebd80a09324fe0c121
Hok Sochetra <chetrafc60@gmail.com>
2024-06-18 21:40:27 +0700

    update

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
 
     production: true,
-    apiUrl      : '139.59.229.32:8002/api',
-    fileUrl     : '139.59.229.32:8003/'
+    apiUrl      : 'http://139.59.229.32:8002/api',
+    fileUrl     : 'http://139.59.229.32:8003/'
 
 };
